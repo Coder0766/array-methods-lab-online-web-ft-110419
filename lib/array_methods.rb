8291500eaa01e5e7f8b51aa7580dd183e73e5792
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-07 18:43:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,13 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
+    array = [num1, num5, num3, num2, num4]
+    array.sort
+end    
 
-end
+using_sort
 
 def using_reverse(array)
 
